27ba1ebe12d39503e13cc85b02f37fc8bf2eaecd
RichardCSantana <richardcsantana@gmail.com>
2015-05-11 00:56:11 -0300

    Criacao da resolucao para o caso do teste

--- cep-modifier/src/main/java/br/com/richard/business/AddressFinder.java
+++ cep-modifier/src/main/java/br/com/richard/business/AddressFinder.java
@@ -23,8 +23,23 @@ public class AddressFinder implements IAddressFinder {
 
 	@Override
 	public Address findByCEP(String cep) {
-		Address result = addressFinder.getAddressByCEP(cep);
+		Address result = null;
+		Integer iteration = 1;
+		do {
+			result = addressFinder.getAddressByCEP(cep);
+			if (result == null) {
+				cep = reviseCEP(cep, iteration);
+				iteration++;
+			}
+		} while (result == null);
 		return result;
 	}
 
+	private String reviseCEP(String cep, Integer iteration) {
+		char[] cepCharArray = cep.toCharArray();
+		int length = cepCharArray.length;
+		cepCharArray[length - iteration] = '0';
+		String result = new String(cepCharArray);
+		return result;
+	}
 }

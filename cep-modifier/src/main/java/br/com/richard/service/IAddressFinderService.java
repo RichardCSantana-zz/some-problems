30cd4f86bce3f1cc9221ee0a65d7647dae5b7be0
RichardCSantana <richardcsantana@gmail.com>
2015-05-11 03:54:05 -0300

    Mudança no recebimento do parametro do serviço

--- cep-modifier/src/main/java/br/com/richard/service/IAddressFinderService.java
+++ cep-modifier/src/main/java/br/com/richard/service/IAddressFinderService.java
@@ -1,7 +1,7 @@
 package br.com.richard.service;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +16,6 @@ public interface IAddressFinderService {
 
 	@RequestMapping("/buscacep")
 	@ResponseBody
-	Address findByCEP(@RequestParam(value = "cep") String cep);
+	Address findByCEP(@RequestBody String cep);
 
 }

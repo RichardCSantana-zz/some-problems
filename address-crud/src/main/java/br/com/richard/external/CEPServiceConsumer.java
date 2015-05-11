df7e31ae75b1ec72f0f1f9f6f0eaa05fd7bdf332
Richard Santana <vrx_rsantana@uolinc.com>
2015-05-11 19:05:46 -0300

    Criado o consumo do servico de busca de CEP

---
+++ address-crud/src/main/java/br/com/richard/external/CEPServiceConsumer.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package br.com.richard.external;
+
+import org.hibernate.mapping.Map;
+import org.springframework.web.client.RestTemplate;
+
+import br.com.richard.model.AddressConsumed;
+
+/**
+ * @author vrx_rsantana
+ *
+ */
+public class CEPServiceConsumer {
+
+	public AddressConsumed getAddressByCEP(String cep) {
+		RestTemplate restTemplate = new RestTemplate();
+		Map<String, String> cep = new HashMap<>();
+		cep.put("cep", "07132322");
+		AddressConsumed address = restTemplate.postForObject(
+				"http://localhost:8080/buscacep", AddressConsumed.class, cep);
+	}
+}

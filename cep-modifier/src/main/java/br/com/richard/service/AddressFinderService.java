fe76e54cdf2256e980180f81069d220b857156bf
RichardCSantana <richardcsantana@gmail.com>
2015-05-12 00:37:35 -0300

    Correcao no status http para erros

--- cep-modifier/src/main/java/br/com/richard/service/AddressFinderService.java
+++ cep-modifier/src/main/java/br/com/richard/service/AddressFinderService.java
@@ -4,9 +4,11 @@
 package br.com.richard.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import br.com.richard.business.IAddressFinder;
 import br.com.richard.model.Address;
@@ -33,6 +35,7 @@ public class AddressFinderService implements IAddressFinderService {
 	}
 
 	@ExceptionHandler(IllegalArgumentException.class)
+	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 	public @ResponseBody String handle(IllegalArgumentException ex) {
 		return ex.getMessage();
 	}
